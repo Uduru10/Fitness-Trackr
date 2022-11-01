f980957e8a9ad6331f0087eb3fa137d3034323f3
Ugonna Duru <Ugoduru@Ugonnas-iMac.local>
2022-11-01 13:26:11 -0700

    still working on seed

---
+++ db/client.js
@@ -0,0 +1,14 @@
+const { Client } = require("pg");
+
+const dbName = "Fitnessdb";
+
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
+
+module.exports = client;
---
+++ db/seed.js
@@ -0,0 +1,43 @@
+const client = require("./client");
+
+// Drop Tables
+const dropTables = async () => {
+  console.log("...dropping tables");
+  await client.query(`
+    DROP TABLE IF EXISTS routine_activities;
+    DROP TABLE IF EXISTS activities;
+    DROP TABLE IF EXISTS routines;
+    DROP TABLE IF EXISTS users;
+    `);
+};
+
+// Create Tables
+const createTables = async () => {
+  console.log(`...creating users table`);
+  await client.query(`
+    CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+    )
+    CREATE TABLE routines(
+        id SERIAL PRIMARY KEY,
+        creator_id INTEGER REFERENCES users(id),
+        is_public BOOLEAN DEFAULT false,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        goal TEXT NOT NULL
+    )
+    CREATE TABLE activities(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT NOT NULL
+    )
+    CREATE TABLE routine_activities(
+        id SERIAL PRIMARY KEY,
+        routine_id INTEGER REFERENCES routine(id),
+        activity_id INTEGER REFERENCES activities (id),
+        duration INTEGER
+        count INTEGER
+        UNIQUE(routine_id, activity_id)
+    )`);
+};
