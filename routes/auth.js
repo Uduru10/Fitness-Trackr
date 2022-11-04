210fdf6c3a0d2615c3591bd69b1564c214543f9c
Ugonna Duru <Ugoduru@Ugonnas-iMac.local>
2022-11-04 12:14:40 -0700

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,3 +3,4 @@ const jwt = require("jsonwebtoken");
 const authRouter = require("express").Router();
 const { User } = require("../db/adapters/models");
 const { JWT_SECRET } = process.env;
+const { authRequired } = require("./utils");
