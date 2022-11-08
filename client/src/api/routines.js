fae3f5dc66da12abff7b72e6c41295b6cc3efd35
bada <bada_0524@gmail.com>
2022-11-08 14:28:19 -0800

    added api

--- client/src/api/routines.js
+++ client/src/api/routines.js
@@ -3,3 +3,21 @@ export async function fetchRoutines() {
   const result = await response.json();
   return result;
 }
+
+export async function createRoutine(is_public, name, goal, token) {
+  const response = await fetch("/api/routines", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      routine: {
+        is_public,
+        name,
+        goal,
+      },
+    }),
+  });
+  const result = await response.json();
+  return result;
+}
