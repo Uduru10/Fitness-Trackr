efb27ef12170de2f98dd666cfa4a72b9f9edcbc3
root <root@Wilson-Desktop.localdomain>
2022-11-15 12:09:44 -0800

    added some boxes

--- client/src/components/RoutineActivity.jsx
+++ client/src/components/RoutineActivity.jsx
@@ -2,6 +2,7 @@ import { fetchRoutines } from "../api/routines";
 import useRoutines from "../hooks/useRoutines";
 
 import { useEffect } from "react";
+import styles from "../styles/RoutineActivity.module.css";
 
 function RoutineActivity() {
   const { routines, setRoutines } = useRoutines([]);
@@ -20,26 +21,27 @@ function RoutineActivity() {
     <>
       {routines.map((routine) => {
         return (
-          <div key={routine.id}>
-            <h5>
-              {" "}
-              Routine #{routine.id}: {routine.name}{" "}
-            </h5>
+          <div className={styles.pairs}>
+            <div key={routine.id}>
+              <h5>
+                {" "}
+                Routine #{routine.id}: {routine.name}{" "}
+              </h5>
 
-            {routine?.activities.map((activity) => {
-              return (
-                <div>
-                  <h6>+Activity #{activity.id}</h6>
-                  <h6>Count: {activity.count}</h6>
-                  <h6>Duration: {activity.duration}</h6>
-                </div>
-              );
-            })}
-            <h6>Made by: {routine.creatorName}</h6>
+              {routine?.activities.map((activity) => {
+                return (
+                  <div>
+                    <h6>+Activity #{activity.id}</h6>
+                    <h6>Count: {activity.count}</h6>
+                    <h6>Duration: {activity.duration}</h6>
+                  </div>
+                );
+              })}
+              <h6>Made by: {routine.creatorName}</h6>
+            </div>
           </div>
         );
       })}
-      <div></div>
     </>
   );
 }
